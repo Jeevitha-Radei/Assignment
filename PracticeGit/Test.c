d5b58ddb917602b7ac9549a01ce3a8a2fe411bea
Jeevitha Ramesh <Jeevitha.Ramesh@trumpf.com>
2024-10-07 11:45:03 +0530

    Main: Separate files

--- PracticeGit/Test.c
+++ PracticeGit/Test.c
@@ -2,12 +2,12 @@
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
-// Program.c
+// test.c
 // ------------------------------------------------------------------------------------------------
 
-#include <stdio.h>
 #include "Program.h"
 
-void PrintMessage () {
-   printf ("Hey!! World\n");
+int main () {
+   PrintMessage ();
+   return 0;
 }
